9e26f3c1cbd69f4eed0d3e578ceacffb7d6e4c02
AsifAlee <asifaly310@gmail.com>
2023-10-21 15:24:42 +0500

    rewards slider added

---
+++ src/components/GameLeaderboartItem.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import borderRight from "../assets/images/decorate-house/leaderboard-no-line.png";
+import unknown from "../assets/images/unknown-user.png";
+import level from "../assets/images/level-img.png";
+import "../styles/game-leaderboard-item.scss";
+import LeaderBoardSlider from "./LeaderBoardSlider";
+import { leaderBoardSliderData } from "../testData";
+const GameLeaderboartItem = () => {
+  return (
+    <div className="game-board-item">
+      <div className="leftDiv">
+        <div className="index">
+          <span>1</span>
+        </div>
+        <img src={borderRight} className="border-img" />
+        <img className="user-avatar" src={unknown} />
+        <div className="user-details">
+          <span>User Name</span>
+          <img src={level} />
+        </div>
+      </div>
+      <div className="right-div">
+        <LeaderBoardSlider rewards={leaderBoardSliderData} />
+      </div>
+    </div>
+  );
+};
+
+export default GameLeaderboartItem;
--- src/pages/DecorateHouse.jsx
+++ src/pages/DecorateHouse.jsx
@@ -1,11 +1,47 @@
 import React from "react";
-
+import title from "../assets/images/decorate-house/heading-text.png";
+import leaderboardTag from "../assets/images/decorate-house/rewards-winners-text.png";
+import "../styles/decorate-house.scss";
+import GameLeaderboartItem from "../components/GameLeaderboartItem";
 const DecorateHouse = () => {
   return (
     <div className="decorate-house">
-      <div className="house-info"></div>
-      <div className="decorate-house-game"></div>
-      <div className="rewards-leaderboard"></div>
+      <div className="decorate-house-game">
+        <button className="reward-hist-btn" />
+      </div>
+      <div className="house-info">
+        <img src={title} className="title" />
+        <div className="info-content">
+          <div className="list-item">
+            <div className="bullet"></div>
+            <span>
+              Use <span className="yellow">FESTIVAL TOKENS</span> collected to
+              purchase new items.
+            </span>
+          </div>
+          <div className="list-item">
+            <div className="bullet"></div>
+            <span>
+              Additional <span className="yellow">BEANS REWARDS</span> will be
+              rewarded for every new purchase you make.
+            </span>
+          </div>
+          <div className="list-item">
+            <div className="bullet"></div>
+            <span>
+              After all the items are purchase,the game will reset,the game will
+              reset.
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="rewards-leaderboard">
+        <img src={leaderboardTag} className="tag" />
+        <div className="winners">
+          <GameLeaderboartItem />
+        </div>
+      </div>
     </div>
   );
 };
